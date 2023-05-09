26fe54ed23ead099f3de07010d8cf91bc169c598
Git git <133003724+git000499@users.noreply.github.com>
2023-05-09 13:40:42 +0000

    Update package.json and App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import "./styles.css";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
 
 export default function App() {
   return (
     <div className="App">
-      1231234
-      <route>
-        <h1>12313</h1>
-      </route>
+      12312345
+      <routes>
+        <route>
+          <h1>12313</h1>
+        </route>
+      </routes>
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
     </div>
